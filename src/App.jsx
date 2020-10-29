a083a24219a0e5ea824a1bb157c197c1c8bbc28f
madhuvanthiram <73576065+madhuvanthiram@users.noreply.github.com>
2020-10-29 16:45:28 +0800

    Add files via upload
    Logics of the project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Keypaddisplay from "./Keypaddisplay"
+import ResultCmp from "./ResultCmp"
+class App extends Component {
+  state = {  }
+  render() {
+
+    return ( 
+   
+     
+<Keypaddisplay/>
+
+     );
+  }
+}
+ 
+export default App;
